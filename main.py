eb1b23df2b6e264eeeba79f0931dc0d2953715cf
n3rddd <119649988+n3rddd@users.noreply.github.com>
2024-08-05 22:55:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,40 @@ from datetime import datetime
 
 # 定义要访问的多个URL
 urls = [
-    'https://raw.githubusercontent.com/iptv-org/iptv/master/streams/cn.m3u',
-    'https://raw.githubusercontent.com/joevess/IPTV/main/iptv.m3u8',
-    'https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt',
-    'https://raw.githubusercontent.com/Guovin/TV/gd/result.txt', #每天自动更新1次
-    'https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt', #每天自动更新1次
-    'https://m3u.ibert.me/txt/fmml_ipv6.txt',
-    'https://m3u.ibert.me/txt/ycl_iptv.txt',
-    'https://m3u.ibert.me/txt/y_g.txt',
-    'https://m3u.ibert.me/txt/j_home.txt',
-    'https://raw.githubusercontent.com/gaotianliuyun/gao/master/list.txt',
-    'https://gitee.com/xxy002/zhiboyuan/raw/master/zby.txt',
-    'https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt', #每天早晚各自动更新1次 2024-06-03 17:50
-    'https://raw.githubusercontent.com/fenxp/iptv/main/live/ipv6.txt',  #1小时自动更新1次11:11 2024/05/13
-    'https://raw.githubusercontent.com/fenxp/iptv/main/live/tvlive.txt', #1小时自动更新1次11:11 2024/05/13
-    'https://raw.githubusercontent.com/zwc456baby/iptv_alive/master/live.txt',  #每天自动更新1次 2024-06-24 16:37
-    'https://gitlab.com/p2v5/wangtv/-/raw/main/lunbo.txt',
-    'https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt'  #ADD 2024-07-22 13:50
+    "https://raw.githubusercontent.com/redrainl/iptv/main/speedtest/zubo_fofa.txt", #ADDED BY LEM ON 01/08/2024
+    "http://tv.850930.xyz/kdsb.m3u", #ADDED BY LEM ON 29/07/2024
+    "http://tv.850930.xyz/kdsb2.m3u", #ADDED BY LEM ON 31/07/2024
+    "http://tv.850930.xyz/gather.m3u", #ADDED BY LEM ON 29/07/2024
+    "http://tv.850930.xyz/543.m3u", #ADDED BY LEM ON 05/08/2024
+    "http://tv.850930.xyz/histar.m3u", #ADDED BY LEM ON 05/08/2024
+    "http://175.178.251.183:6689/channel.txt",
+    "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
+    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
+    "http://120.79.4.185/new/mdlive.txt",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
+    "https://live.zhoujie218.top/tv/iptv6.txt",
+    "https://tv.youdu.fan:666/live/",
+    "http://ww.weidonglong.com/dsj.txt",
+    "http://xhztv.top/zbc.txt",
+    "https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt",
+    "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
+    "https://v.nxog.top/m/tv/1/",
+    "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
+    "http://home.jundie.top:81/Cat/tv/live.txt",
+    "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
+    "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
+    "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
+    "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
+    "https://fm1077.serv00.net/SmartTV.m3u",
+    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
+    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt", #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt", #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/yoursmile66/TVBox/main/live.txt", #ADDED BY LEM ON 29/07/2024
+    "http://ttkx.live:55/lib/kx2024.txt", #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u", #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/Love4vn/love4vn/main/Sport.m3u", #奥运 ON 29/07/2024
+    "https://gitlab.com/tvkj/qxitv/-/raw/main/888.txt" #ADDED BY LEM ON 29/07/2024
 ]
 
 #read BlackList 2024-06-17 15:02
